0b03a3659b9314797e3e63c747fa318af3725c97
mewin <>
2012-10-18 22:14:58 +0200

    regions now save on /region save

--- com/mewin/WGCustomFlags/WGCustomFlagsListener.java
+++ com/mewin/WGCustomFlags/WGCustomFlagsListener.java
@@ -4,8 +4,10 @@
  */
 package com.mewin.WGCustomFlags;
 
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldSaveEvent;
@@ -45,5 +47,31 @@ public class WGCustomFlagsListener implements Listener {
     public void onWorldUnload(WorldUnloadEvent e)
     {
         this.plugin.saveFlagsForWorld(e.getWorld());
+    }
+    
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent e)
+    {
+        String msg = e.getMessage().toLowerCase().trim();
+        if (!e.getPlayer().hasPermission("worldguard.region.save") || (!msg.startsWith("/rg save") && !msg.startsWith("/region save")))
+        {
+            return;
+        }
+        
+        String[] split = msg.split(" ");
+        
+        if (split.length <= 2)
+        {
+            plugin.saveAllWorlds();
+        }
+        else
+        {
+            World w = plugin.getServer().getWorld(split[2]);
+            
+            if (w != null)
+            {
+                plugin.saveFlagsForWorld(w);
+            }
+        }
     }
 }
