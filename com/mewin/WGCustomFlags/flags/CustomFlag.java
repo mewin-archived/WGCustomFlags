f911f94ce305afdd0c98fbd6a5e877dbd810bb6e
BangL <henno.rickowski@googlemail.com>
2012-10-21 15:37:10 +0200

    GPLv3.0

--- com/mewin/WGCustomFlags/flags/CustomFlag.java
+++ com/mewin/WGCustomFlags/flags/CustomFlag.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 mewin <mewin001@hotmail.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.mewin.WGCustomFlags.flags;
 
 import com.sk89q.worldguard.protection.flags.Flag;
@@ -6,7 +22,7 @@ import com.sk89q.worldguard.protection.flags.RegionGroup;
 /**
  * Sub class for custom flags for WorldGuard custom flags
  * allways use this class as sub class if you want your flags to be saved
- * @author mewin
+ * @author mewin <mewin001@hotmail.de>
  */
 public abstract class CustomFlag<T> extends Flag<T> {
     public CustomFlag(String name, RegionGroup rg) {
--- com/mewin/WGCustomFlags/util/ClassHacker.java
+++ com/mewin/WGCustomFlags/util/ClassHacker.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 mewin <mewin001@hotmail.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.mewin.WGCustomFlags.util;
 
 import java.lang.reflect.Field;
@@ -5,7 +21,7 @@ import java.lang.reflect.Modifier;
 
 /**
  *
- * @author mewin
+ * @author mewin <mewin001@hotmail.de>
  */
 public final class ClassHacker {
 
