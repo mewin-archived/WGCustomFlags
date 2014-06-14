a53c36fb7f96d5c5cdb78b0490c42972d0aa87af
Mewin <mewin001@hotmail.de>
2014-06-14 14:39:32 +0200

    added package-infos

---
+++ src/main/java/com/mewin/WGCustomFlags/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) by mewin
+ * All rights reserved.
+ */
+
+/**
+ * main package for the most important WGCustomFlags classes
+ * @author mewin<mewin@mewin.de>
+ */
+package com.mewin.WGCustomFlags;
---
+++ src/main/java/com/mewin/util/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) by mewin
+ * All rights reserved.
+ */
+
+/**
+ * utility classes that are created to be used by other plugins
+ */
+package com.mewin.util;
